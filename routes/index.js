1e0a22abdfbab364d84b3c06fe6e6f9fd07240bd
Lawrence Tran <lawrence@tran.com>
2016-04-04 00:26:59 -0500

    added put update.

--- routes/index.js
+++ routes/index.js
@@ -39,4 +39,37 @@ router.post('/api/hotscore', function(req, res) {
   });
 });
 
+// Update Item
+router.put('/api/hotscore/:hotscore_id', function(req, res) {
+
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.hotscore_id;
+
+  // Grab data from http request
+  var data = {title: req.body.title, likes: req.body.likes, age: req.body.age}
+
+  // Get a Postgres client from the conenction pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).send(json({ success: false, data: err}));
+    }
+
+    // SQL Query > Update Data
+    client.query("UPDATE items SET title=($1), likes=($2), age=($3) WHERE id=($4)", [data.title, data.likes, data.age, id], function(err, result){
+      if (err) {
+        return res.status(500).json({success: false, err});
+      } else {
+        return res.status(200).json({success: true});
+      }
+    });
+
+  });
+});
+
+
 module.exports = router;
